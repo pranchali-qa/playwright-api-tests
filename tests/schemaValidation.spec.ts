034b260d1cd7ac36ba040b618fd30a264d98fa34
pranchali-qa <pranchaliaher@gmail.com>
2026-08-17 13:33:35 +0530

    Implement schema validation test using Playwright
    Add schema validation test for JSON response

---
+++ tests/schemaValidation.spec.ts
@@ -0,0 +1,15 @@
+import {test,expect} from "@playwright/test"
+import Ajv from 'ajv'
+import fs from 'fs'
+
+test('schema validation test', async ({request}){
+
+  const response = await request.get('https://mocktarget.apigee.net/json');
+  const response_body = await response.json();
+  console.log(response_body);
+
+  const ajv = new Ajv();
+  const validate=ajv.compile(schema);
+  const isValid = validate(response_body);
+  expect(isValid).toBeTruthy();
+});
