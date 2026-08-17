750b00022cf45ea6e5d78e436ab03c8a0a40e46b
pranchali-qa <pranchaliaher@gmail.com>
2026-08-17 13:34:43 +0530

    Update schemaValidation.spec.ts

--- tests/schemaValidation.spec.ts
+++ tests/schemaValidation.spec.ts
@@ -2,7 +2,7 @@ import {test,expect} from "@playwright/test"
 import Ajv from 'ajv'
 import fs from 'fs'
 
-test('schema validation test', async ({request}){
+test('schema validation test', async ({request}) => {
 
   const response = await request.get('https://mocktarget.apigee.net/json');
   const response_body = await response.json();
